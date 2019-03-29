ada5ec8480918f190745596f8118a64f51130ddd
Jason Kim <jasonkim4201@gmail.com>
2019-03-28 22:16:56 -0400

    have it made so burger ingredients can be added dymamically once controls are implemented

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,25 +3,21 @@ import classes from "./Burger.module.css";
 import BurgerIngredients from "./BurgerIngredients/BurgerIngredients";
 
 const Burger = (props) => {
-  return (
+    // Object.keys() extracts keys of a given objectand turns it into an array. so array of keys
+    // so bacon, cheese and meat will come out as a string which will help make this all dynamically
+    const transformedIngredients = Object.keys(props.ingredients).map(ingredientKey => {     
+          // spread a new array with array method
+          //ie [...Array(3)] would give me an array of 3 empty spaces with 3 undefined spaces. makes sense right?!?
+          return [...Array(props.ingredients[ingredientKey])].map((unusedElement, i) => {
+                                //key would look like bacon2 or whatever...
+              return <BurgerIngredients key={ingredientKey + i} type={ingredientKey} />;
+          });
+        });
+
+return (
     <div className={classes.Burger}>
         <BurgerIngredients type="bread-top" />
-        <BurgerIngredients type="bacon" />
-        <BurgerIngredients type="cheese" />
-        <BurgerIngredients type="meat" />
-        <BurgerIngredients type= "salad" />
-        <BurgerIngredients type="cheese" />
-        <BurgerIngredients type="meat" />
-        <BurgerIngredients type="cheese" />
-        <BurgerIngredients type="meat" />
-        <BurgerIngredients type= "salad" />
-        <BurgerIngredients type="cheese" />
-        <BurgerIngredients type="meat" />
-        <BurgerIngredients type="cheese" />
-        <BurgerIngredients type="meat" />
-        <BurgerIngredients type= "salad" />
-        <BurgerIngredients type="cheese" />
-        <BurgerIngredients type="meat" />
+        {transformedIngredients}
         <BurgerIngredients type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,12 +5,21 @@ import Burger from "../../components/Burger/Burger";
 
 
 class BurgerBuilder extends Component {
-  state = {}
+  state = {
+    ingredients: {
+        bacon: 1,
+        salad: 1,
+        cheese: 2,
+        meat: 2,
+        
+    },
+    price: 10000
+  }
 
   render() {
     return (
       <Auxiliary>
-        <Burger />
+        <Burger  ingredients={this.state.ingredients}/>
         <div>Build Controls</div>
       </Auxiliary>
     )
