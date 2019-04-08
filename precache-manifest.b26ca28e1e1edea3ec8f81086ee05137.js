0a6b9d007dba3f2f1bc2bea57acb0447bb75e5b7
Jason Kim <jasonkim4201@gmail.com>
2019-04-08 02:37:01 -0400

    Updates

---
+++ precache-manifest.b26ca28e1e1edea3ec8f81086ee05137.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9d464b5c1801a38f7e6",
+    "url": "/basic-burger-builder/static/js/4.88a259cd.chunk.js"
+  },
+  {
+    "revision": "c13420938caa9aae2cc1",
+    "url": "/basic-burger-builder/static/css/main.aceb7fa7.chunk.css"
+  },
+  {
+    "revision": "2ac58d641ca13d312d87",
+    "url": "/basic-burger-builder/static/js/runtime~main.9984d787.js"
+  },
+  {
+    "revision": "f9b299f7b494e8624559",
+    "url": "/basic-burger-builder/static/js/2.22fb8e47.chunk.js"
+  },
+  {
+    "revision": "40d3abbd9174e4a0becca25eb1b17ffa",
+    "url": "/basic-burger-builder/static/media/kissclipart-cartoon-clipart-cheeseburger-veggie-burger-junk-fo-58c1153b58284680.40d3abbd.png"
+  },
+  {
+    "revision": "75ae2a835d7a04d88eb2",
+    "url": "/basic-burger-builder/static/js/3.57c21ef5.chunk.js"
+  },
+  {
+    "revision": "bf6633d227a7c9b132236e707d9ef133",
+    "url": "/basic-burger-builder/static/media/burger-guard.bf6633d2.png"
+  },
+  {
+    "revision": "c13420938caa9aae2cc1",
+    "url": "/basic-burger-builder/static/js/main.67ff6fe9.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/basic-burger-builder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "c34054f79a94e62a0b37",
+    "url": "/basic-burger-builder/static/js/5.c7d47eeb.chunk.js"
+  },
+  {
+    "revision": "c34054f79a94e62a0b37",
+    "url": "/basic-burger-builder/static/css/5.5228d980.chunk.css"
+  },
+  {
+    "revision": "f9d464b5c1801a38f7e6",
+    "url": "/basic-burger-builder/static/css/4.e7f4fee4.chunk.css"
+  },
+  {
+    "revision": "75ae2a835d7a04d88eb2",
+    "url": "/basic-burger-builder/static/css/3.522ef086.chunk.css"
+  },
+  {
+    "revision": "eca3dffba40d859f3806b7125cd3a0f5",
+    "url": "/basic-burger-builder/index.html"
+  }
+];
